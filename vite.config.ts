28c629a4a29cb3591b05617bddd6b7441321b388
Aisbondeng <kpphmri@gmail.com>
2025-04-15 14:39:53 +0800

    Fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-// ~/Langit-Pranaja/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: './client',
-  base: '/Langit-Pranaja/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'client/src') // Menetapkan alias '@' ke folder 'src'
-    }
-  }
+  base: '/Langit-Pranaja/', // <- Penting untuk GitHub Pages
 })
